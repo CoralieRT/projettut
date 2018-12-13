4deaed8ce5a1b35c345b980ae399dbd3b0f1d139
CoralieRT <34239901+CoralieRT@users.noreply.github.com>
2018-12-13 14:27:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,62 @@
     <body>
         <header> Département R&T FI1A </header>
         <h1>Gestion des absences</h1>
-         <!--formulaire qui envoie les information vers le script d'authentification : login et mdp-->
-        <form method="post" action="connect.php">
+		<body><div id="maposition"></div>
+
+			<script src="https://maps.google.com/maps/api/js?sensor=false"></script>
+			<script src="jquery-3.3.1.js"></script>
+			<script>
+				// Position par défaut
+				var centerpos = new google.maps.LatLng(48.657393,-1.969133);
+
+				if(navigator.geolocation) {
+							
+					// Fonction de callback en cas de succès
+					function affichePosition(position) {					
+						var infopos = "Position déterminée : <br>";
+						infopos += "Latitude : "+position.coords.latitude +"<br>";
+						infopos += "Longitude: "+position.coords.longitude+"<br>";
+						infopos += "Altitude : "+position.coords.altitude +"<br>";
+						document.getElementById("maposition").innerHTML = infopos;
+						console.log(position.coords.latitude);
+						
+						// On instancie un nouvel objet LatLng pour Google Maps
+						var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+						$("#lat").val(position.coords.latitude); 
+						$("#lon").val(position.coords.longitude);
+
+						// Fonction de callback en cas d’erreur
+					function erreurPosition(error) {
+						var info = "Erreur lors de la géolocalisation : ";
+						switch(error.code) {
+						case error.TIMEOUT:
+							info += "Timeout !";
+						break;
+						case error.PERMISSION_DENIED:
+							info += "Vous n’avez pas donné la permission";
+						break;
+						case error.POSITION_UNAVAILABLE:
+							info += "La position n’a pu être déterminée";
+						break;
+						case error.UNKNOWN_ERROR:
+							info += "Erreur inconnue";
+						break;
+						}
+						document.getElementById("maposition").innerHTML = info;
+					}
+
+					navigator.geolocation.getCurrentPosition(affichePosition,erreurPosition);
+				} 
+				else {
+
+					alert("Ce navigateur ne supporte pas la géolocalisation");
+				}
+			</script>
+         <!--formulaire qui envoie les information vers le script d'authentification : géoloc, login et mdp-->
+		    <form method="post" action="connect.php">
             <fieldset>
+			<input type="hidden" name="lat" id="lat" />
+			<input type="hidden" name="lon" id="lon" />
             <input type="text" name="login" placeholder="N° SESAME"/> <br/> 
             <input type="password" name="mdp" placeholder="Mot de passe" /> <br/>
             <br/>
