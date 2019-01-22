1408de391b2b5bc8d705f5c290b34957b41c02da
OlivierRogard <41162270+OlivierRogard@users.noreply.github.com>
2019-01-22 11:53:30 +0100

    Update justif_abs.php

--- justif_abs.php
+++ justif_abs.php
@@ -2,7 +2,7 @@
 	session_start();
 	include('bdd_connect.php');
 	
-	if(!is_dir('justificatifs')){
+	if(!is_dir('justificatifs')){		//création du fichier s'il n'existe pas
 		mkdir('justificatifs');
 	}
 	/*on récupère le fichier avec la variable globale $_FILES*/
@@ -12,7 +12,7 @@
 	$fichier = $_FILES['file']['name'];	//on récupère le nom du fichier pdf envoyé
 
 	$extension = strtolower(  substr(  strrchr($fichier, '.')  ,1)  );			//on vérifie son extension
-	if ( $extension!='pdf' ) 
+	if ( $extension!='pdf' ) 	//envoi du fichier seulement 
 		echo "Extension incorrecte";
 	
 	/*téléchargement du fichier sur notre serveur*/
@@ -22,17 +22,17 @@
 	$name = basename($fichier);
 	move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
 	$loginetu=$_SESSION['login'];
-	foreach($_POST['absence'] as $valeur){
+	foreach($_POST['absence'] as $valeur){		//implémentation des jsutificaits dans la bdd avec les différents paramètres
 		$date=$valeur;
 		$heure=substr($date,11,18);
 		$date=substr($date,0,10);	
 		if (($heure<="12:15:00") &&($heure>="08:00:00"))
-			$date=$date." matinée";
-       		if (($heure<="19:00:00") &&($heure>="13:45:00"))  
+			$date=$date." matin";
+        if (($heure<="19:00:00") &&($heure>="13:45:00"))  
 			$date=$date." après-midi";
 		$heureact=date('H')+1;
 		$dateact=date('Y-m-d ').$heureact.date(':i');
-		$req = $bdd->prepare("INSERT INTO `justificatif` (`loginetu`, `dateabs`, `filename`, `date`) VALUES (:logetu,:dt,:name,:dtact)"); 
+		$req = $bdd->prepare("INSERT INTO `justificatif` (`loginetu`, `dateabs`, `filename`, `date`) VALUES (:logetu,:dt,:name,:dtact)"); 	
 		$req->execute(array('logetu'=>$loginetu,'dt'=>$date,'name'=>$name,'dtact'=>$dateact));
 		header('Location: justificatif.php');
 	}
