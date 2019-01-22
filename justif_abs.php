ac8a7d3d07972d1cd72508a333ebf3323c06c99b
OlivierRogard <41162270+OlivierRogard@users.noreply.github.com>
2019-01-22 10:52:33 +0100

    Update justif_abs.php

--- justif_abs.php
+++ justif_abs.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	include('bdd_connect.php');
-	ob_start();
+	
 	if(!is_dir('justificatifs')){
 		mkdir('justificatifs');
 	}
@@ -9,7 +9,7 @@
 	if ($_FILES['file']['error'] > 0) 
 		echo "Erreur lors du transfert";
 
-	$fichier = "justificatifs\\".$_FILES['file']['name'];	//on récupère le nom du fichier pdf envoyé
+	$fichier = $_FILES['file']['name'];	//on récupère le nom du fichier pdf envoyé
 
 	$extension = strtolower(  substr(  strrchr($fichier, '.')  ,1)  );			//on vérifie son extension
 	if ( $extension!='pdf' ) 
@@ -21,15 +21,19 @@
 	$uploadfile = $uploaddir . basename($_FILES['file']['name']);
 	$name = basename($fichier);
 	move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
-	$loginetu=$_SESSION['login'];echo $loginetu;
-	$req = $bdd->prepare("SELECT * FROM etudiant WHERE login=? ");
-	$req->execute(array($loginetu));
-	$date=$_POST['absence'];
-	while($test=$req->fetch()){
-	    $classe=$test['id_promo'];}
-	   echo $classe;
-	$req = $bdd->prepare("INSERT INTO `justificatif` (`loginetu`, `dateabs`, `filename`,`classe`) VALUES (:logetu,:dt,:name,:classe)"); 
-	$req->execute(array('logetu'=>$loginetu,'dt'=>$date,'name'=>$name,'classe'=>$classe));
-	header('Location: justificatif.php');
-	ob_end_flush();
+	$loginetu=$_SESSION['login'];
+	foreach($_POST['absence'] as $valeur){
+		$date=$valeur;
+		$heure=substr($date,11,18);
+		$date=substr($date,0,10);	
+		if (($heure<="12:15:00") &&($heure>="08:00:00"))
+			$date=$date." matinée";
+        if (($heure<="19:00:00") &&($heure>="13:45:00"))  
+			$date=$date." après-midi";
+		$heureact=date('H')+1;
+		$dateact=date('Y-m-d ').$heureact.date(':i');
+		$req = $bdd->prepare("INSERT INTO `justificatif` (`loginetu`, `dateabs`, `filename`, `date`) VALUES (:logetu,:dt,:name,:dtact)"); 
+		$req->execute(array('logetu'=>$loginetu,'dt'=>$date,'name'=>$name,'dtact'=>$dateact));
+		header('Location: justificatif.php');
+	}
 ?>
